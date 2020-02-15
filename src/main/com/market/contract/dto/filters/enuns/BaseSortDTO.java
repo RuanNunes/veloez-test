78a6c5aac5f423a96016da194c97ec2ccbf3f60d
Ruan Nunes <ruanschwanz@gmail.com>
2020-02-15 18:53:04 -0300

    limpando classes de auxilio

--- src/main/com/market/contract/dto/filters/enuns/BaseSortDTO.java
+++
@@ -1,6 +0,0 @@
-package com.market.contract.dto.filters.enuns;
-
-public enum BaseSortDTO {
-    MOST_RECENT,
-    LEAST_RECENT,
-}
--- src/main/com/market/contract/dto/filters/enuns/RuleMarketPlaceSortDTO.java
+++
@@ -1,6 +0,0 @@
-package com.market.contract.dto.filters.enuns;
-
-public enum RuleMarketPlaceSortDTO {
-    MOST_RECENT,
-    LEAST_RECENT,
-}
